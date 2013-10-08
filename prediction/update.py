3a4286ff262226246be5b69fa6187bd8ebb84aaf
Peter Bouda <pbouda@cidles.eu>
2013-10-08 14:06:39 +0100

    Updated prediction update script for pressagio. Changed from config package to configparser modile.

--- prediction/update.py
+++ prediction/update.py
@@ -15,21 +15,24 @@ import shutil
 import codecs
 import re
 from tempfile import mkdtemp
+import configparser
 
-from config import Config
+import pressagio.tokenizer
+import pressagio.dbconnector
 
 ###################################### Main
 
 def main(argv):
     config_file = file(os.path.join('..', 'config.ini'))
-    config = Config(config_file)
+    config = configparser.ConfigParser()
+    config.read(config_file)
 
     corpus_dir = os.path.abspath(os.path.join("..", "build", "corpus"))
     prediction_dir = os.path.join("..", "build", "prediction")
-    for l in config['supported_languages']:
+    for iso_639_3 in config["LanguagesISOMap"]:
         
         sql_file = os.path.join(prediction_dir, "{0}.sqlite".format(
-            l['iso_639_3']))
+            iso_639_3))
 
         if os.path.exists(sql_file):
             os.remove(sql_file)
@@ -44,7 +47,7 @@ def main(argv):
 
         for f in glob.glob(
                 os.path.join(corpus_dir, "{0}wiki*.zip".format(
-                    l['iso_639_1']))):
+                    config["LanguagesISOMap"][iso_639_3]))):
             
             _, filename = os.path.split(os.path.abspath(f))
             filebasename, _ = os.path.splitext(filename)
@@ -60,12 +63,21 @@ def main(argv):
                 text_file = os.path.join(
                     tmp_path,"{0}.txt".format(filebasename))
 
-                # build n-gramm statistics, requires presage:
-                # http://presage.sourceforge.net/
-                for gram in [1, 2, 3]:
-                    os.system(
-                        "text2ngram -n {0} -o {1} -f sqlite -a {2}".format(
-                            gram, sql_file, text_file))
+                # build n-gramm statistics, requires pressagio:
+                # http://github.com/cidles/pressagio
+                for ngram_size in [1, 2, 3]:
+                    print("  Parsing {0} for cardinality {1}...".format(
+                        text_file, ngram_size))
+                    ngram_map = pressagio.tokenizer.forward_tokenize_file(
+                        text_file, ngram_size, True)
+
+                    print("  Writing result to {0}...".format(sql_file))
+                    pressagio.dbconnector.insert_ngram_map(ngram_map,
+                        ngram_size, sql_file, False)
+
+                    #os.system(
+                    #    "text2ngram -n {0} -o {1} -f sqlite -a {2}".format(
+                    #        gram, sql_file, text_file))
 
                 # append words to dictionary list
                 doc = ""
@@ -81,15 +93,15 @@ def main(argv):
 
         # write dictionary file
         dict_file = os.path.join(prediction_dir, "{0}_dict.txt".format(
-            l['iso_639_3']))
+            iso_639_3))
         with codecs.open(dict_file, "w", "utf-8") as f:
             f.write("\n".join(dictionary))
 
         # write config file
         config_template = u""
-        with codecs.open("config_template.xml", "r", "utf-8") as f:
+        with codecs.open("config_template.ini", "r", "utf-8") as f:
             config_template = f.read()
-        config_file = os.path.join(prediction_dir, "{0}.xml".format(
+        config_file = os.path.join(prediction_dir, "{0}.ini".format(
             l['iso_639_3']))
         with codecs.open(config_file, "w", "utf-8") as f:
             f.write(config_template.format(l['iso_639_3']))
