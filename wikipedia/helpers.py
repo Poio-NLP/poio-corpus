e76049fb58ff831629e060591f46fa69548b8afb
Peter Bouda <pbouda@cidles.eu>
2013-10-10 11:47:53 +0100

    Refactored update scripts for wikipedia and prediction.

--- wikipedia/helpers.py
+++ wikipedia/helpers.py
@@ -15,6 +15,7 @@ import zipfile
 import re
 import urllib2
 import urlparse
+import subprocess
 
 import requests
 from BeautifulSoup import BeautifulSoup
@@ -24,41 +25,49 @@ def dump_link(wiki_name, page):
     soup = BeautifulSoup(html_page)
     all_links = soup('a')
     for l in all_links:
-        match = re.match(wiki_name + "-(\d{8})-pages-articles.xml.bz2", l.string)
+        match = re.match(
+            wiki_name + "-(\d{8})-pages-articles.xml.bz2", l.string)
         if match:
             wiki_date = match.group(1)
             dump_link = urlparse.urljoin(page, l['href'])
             return wiki_date, dump_link
     return None, None
             
-def download_dump(dump_link, wiki_name):
+def download_dump(dump_link, wiki_name, new_wiki_name):
     file_name = dump_link.split('/')[-1]
-    file_path = os.path.join(wiki_name, file_name)
+    file_path = os.path.join(new_wiki_name, file_name)
     if not os.path.exists(file_path):
         r = requests.get(dump_link)
         with open(file_path, "wb") as f:
             f.write(r.content)
     return file_path
 
-def wikipedia_extractor(file_path, wiki_name):
-    os.system("{0} WikiExtractor.py -w -f tanl {1} {2}/extracted".format(
+def wikipedia_extractor(file_path, new_wiki_name):
+    proc = subprocess.Popen([
         sys.executable,
+        "WikiExtractor.py",
+        "-w",
+        "-f", "tanl",
         file_path,
-        wiki_name))
+        os.path.join(new_wiki_name, "extracted")
+        ], stdout=subprocess.PIPE, shell=True)
+    (out, err) = proc.communicate()
+    return (out, err)
         
         
-def concatenate(wiki_name):
-    filenames = glob.glob(os.path.join(wiki_name, "extracted", "*.raw"))
+def concatenate(new_wiki_name):
+    filenames = glob.glob(os.path.join(new_wiki_name, "extracted", "*.raw"))
     with codecs.open(os.path.join(
-            wiki_name, "{0}.xml".format(wiki_name)), 'w', 'utf-8') as outfile:
+            new_wiki_name, "{0}.xml".format(new_wiki_name)), 'w', 'utf-8')\
+            as outfile:
         for fname in filenames:
             with codecs.open(fname, "r", "utf-8") as infile:
                 for line in infile:
                     outfile.write(line)
                     
-def clean_1(wiki_name):
+def clean_1(new_wiki_name):
     os.system("{0} clean.py {1} {2}".format(
         sys.executable,
-        os.path.join(wiki_name, "{0}.xml".format(wiki_name)),
-        os.path.join(wiki_name, "{0}_cleaned1.xml".format(wiki_name))))
+        os.path.join(new_wiki_name, "{0}.xml".format(new_wiki_name)),
+        os.path.join(new_wiki_name, "{0}_cleaned1.xml".format(new_wiki_name))))
 
--- wikipedia/update.py
+++ wikipedia/update.py
@@ -15,74 +15,118 @@ import codecs
 import urllib2
 import urlparse
 import zipfile
+try:
+    import configparser
+except ImportError:
+    import ConfigParser as configparser
 
 import requests
 from BeautifulSoup import BeautifulSoup
 
 import helpers
 
-languages = os.walk('.').next()[1]
+###################################### Main
 
-url = "http://dumps.wikimedia.org/backup-index.html"
-html_page = urllib2.urlopen(url)
-soup = BeautifulSoup(html_page)
+def main(argv):
+    config_file = os.path.join('..', 'config.ini')
+    config = configparser.ConfigParser()
+    config.read(config_file)
 
-lang_pages = [(link.string, urlparse.urljoin(url, link['href']))
-                for l in languages
-                    for link in soup('a')
-                        if link.string == l]
+    for iso_639_3 in config.options("LanguagesISOMap"):
+        iso_639_1 = config.get("LanguagesISOMap", iso_639_3)
+        wiki_prefix = "{0}wiki".format(iso_639_1)
+        new_wiki_prefix = "{0}wiki".format(iso_639_3)
 
-for wiki_name, page in lang_pages:
-    wiki_date, dump_link = helpers.dump_link(wiki_name, page)
+        print("Processing wikipedia {0} -> {1}...".format(iso_639_1, iso_639_3))
 
-    if not dump_link:
-        print("Could not find dump link for {0}.".format(wiki_name))
-        sys.exit(1)
+        # check if we already have a clean script for this language
+        if not os.path.exists(os.path.join(new_wiki_prefix, "clean2.py")):
+            print("No clean script found. Skipping.")
+            continue
 
-    print("Downloading {0}...".format(dump_link))
-    file_path = helpers.download_dump(dump_link, wiki_name)
-   
-    
-    helpers.wikipedia_extractor(file_path, wiki_name)
+        url = "http://dumps.wikimedia.org/backup-index.html"
+        html_page = urllib2.urlopen(url)
+        soup = BeautifulSoup(html_page)
 
-    # Concatenate output files
-    helpers.concatenate(wiki_name)
+        page = None
+        for link in soup('a'):
+            if link.string == wiki_prefix:
+                page = urlparse.urljoin(url, link['href'])
 
-    # Calling clean scripts
-    print("Cleaning...")
-    helpers.clean_1(wiki_name)   
-   
+        # get the link for the dump file
+        wiki_date, dump_link = helpers.dump_link(wiki_prefix, page)
 
-    os.system("{0} {1}/clean2.py {2} {3}".format(
-        sys.executable,
-        wiki_name,
-        os.path.join(wiki_name, "{0}_cleaned1.xml".format(wiki_name)),
-        os.path.join(wiki_name, "{0}_cleaned2.xml".format(wiki_name))))
+        if not dump_link:
+            sys.stderr.write("Could not find dump link. Abort.")
+            sys.exit(1)
 
-    os.system("{0} clean3.py {1} {2}".format(
-        sys.executable,
-        os.path.join(wiki_name, "{0}_cleaned2.xml".format(wiki_name)),
-        os.path.join(wiki_name, "{0}_cleaned3.xml".format(wiki_name))))
+        # check if there is already build for this Wikipedia dump
+        output_file = os.path.join(
+            '..', 'build', 'corpus', "{0}-{1}.zip".format(new_wiki_prefix,
+                wiki_date))
+        if os.path.exists(output_file):
+            print("Output file already exists. Skipping.")
+            continue
 
-    print("Converting to GrAF...")
-    os.system("{0} to_graf.py {1} {2}".format(
-        sys.executable,
-        os.path.join(wiki_name, "{0}_cleaned3.xml".format(wiki_name)),
-        os.path.join(wiki_name, "{0}-{1}.hdr".format(wiki_name, wiki_date))))
+        # download dump
+        print("Downloading {0}...".format(dump_link))
+        file_path = helpers.download_dump(dump_link, wiki_prefix,
+            new_wiki_prefix)
+       
+        print("Running WikiExtractor...")
+        helpers.wikipedia_extractor(file_path, new_wiki_prefix)
 
-    # Zipping
-    print("Zipping...")
-    files = [
-        os.path.join(wiki_name, "{0}-{1}.hdr".format(wiki_name, wiki_date)),
-        os.path.join(wiki_name, "{0}-{1}.txt".format(wiki_name, wiki_date)),
-        os.path.join(wiki_name, "{0}-{1}-doc.xml".format(wiki_name, wiki_date))
-    ]
-    myzip = zipfile.ZipFile(os.path.join(
-        '..', 'build', 'corpus', "{0}-{1}.zip".format(wiki_name, wiki_date)), 
-        'w', zipfile.ZIP_DEFLATED)
-    for f in files:
-        myzip.write(f, os.path.basename(f))
-    myzip.write("LICENSE.wikipedia", "LICENSE")
-    myzip.close()
+        # Concatenate output files
+        helpers.concatenate(new_wiki_prefix)
 
-    print
+        # Calling clean scripts
+        print("Cleaning...")
+        helpers.clean_1(new_wiki_prefix)   
+       
+        os.system("{0} {1}/clean2.py {2} {3}".format(
+            sys.executable,
+            new_wiki_prefix,
+            os.path.join(
+                new_wiki_prefix, "{0}_cleaned1.xml".format(new_wiki_prefix)),
+            os.path.join(
+                new_wiki_prefix, "{0}_cleaned2.xml".format(new_wiki_prefix))))
+
+        os.system("{0} clean3.py {1} {2}".format(
+            sys.executable,
+            os.path.join(
+                new_wiki_prefix, "{0}_cleaned2.xml".format(new_wiki_prefix)),
+            os.path.join(
+                new_wiki_prefix, "{0}_cleaned3.xml".format(new_wiki_prefix))))
+
+        print("Converting to GrAF...")
+        os.system("{0} to_graf.py {1} {2}".format(
+            sys.executable,
+            os.path.join(
+                new_wiki_prefix, "{0}_cleaned3.xml".format(new_wiki_prefix)),
+            os.path.join(
+                new_wiki_prefix, "{0}-{1}.hdr".format(
+                    new_wiki_prefix, wiki_date))))
+
+        # Zipping
+        print("Zipping...")
+        files = [
+            os.path.join(
+                new_wiki_prefix, "{0}-{1}.hdr".format(
+                    new_wiki_prefix, wiki_date)),
+            os.path.join(
+                new_wiki_prefix, "{0}-{1}.txt".format(
+                    new_wiki_prefix, wiki_date)),
+            os.path.join(
+                new_wiki_prefix, "{0}-{1}-doc.xml".format(
+                    new_wiki_prefix, wiki_date))
+        ]
+        myzip = zipfile.ZipFile(output_file, 'w', zipfile.ZIP_DEFLATED)
+        for f in files:
+            myzip.write(f, os.path.basename(f))
+        myzip.write("LICENSE.wikipedia", "LICENSE")
+        myzip.close()
+
+        print
+
+if __name__ == "__main__":
+    main(sys.argv)
