1e3400e07ef9b67b4c44f623db685a6cc23658e9
Peter Bouda <pbouda@cidles.eu>
2013-11-05 11:52:07 +0000

    Deleted site dir, moved to project poio-site.

--- site/flask-app/gunicorn.conf.py
+++
@@ -1,8 +0,0 @@
-import os
-
-bind = "127.0.0.1:8200"
-workers = (os.sysconf("SC_NPROCESSORS_ONLN") * 2) + 1
-#loglevel = "error"
-proc_name = "poio"
-logfile = '/home/sites/poio/site/flask-app/debug.log'
-loglevel = 'debug'
--- site/flask-app/src/main/static/separators/README.rst
+++
@@ -1,1 +0,0 @@
-Directory for separator files.
