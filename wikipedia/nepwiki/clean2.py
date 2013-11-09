c2718257112275b8ae6dba96ba09c43384e331a5
evaschinzel <eva_schinzel@gmx.de>
2013-11-09 01:32:42 +0100

    added clean2 scripts for manipuri and nepali language

---
+++ wikipedia/nepwiki/clean2.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+#
+# Poio Corpus
+#
+# Copyright (C) 2009-2013 Poio Project
+# Author: Eva Schinzel
+# URL: <http://media.cidles.eu/poio/>
+# For license information, see LICENSE.TXT
+
+# clean2-script for Nepali language
+
+# in order to run this script separately, enter in command line as follows:
+# (while being located in directory "nepwiki") 
+# c:\Python27\python.exe clean2.py nepwiki_cleaned1.xml nepwiki_cleaned2.xml
+
+# in order to test this script, enter in python shell as follows:
+# >>> import xml.etree.ElementTree as ET
+# >>> ET.parse("nepwiki_cleaned2.xml")
+
+import sys
+import re
+import codecs
+
+if not len(sys.argv) == 3:
+    print("In order to run this script, enter in command line as follows: clean2.py nepwiki_cleaned1.xml nepwiki_cleaned2.xml")
+    sys.exit(1)
+
+    
+def main(argv):
+    f1 = codecs.open(argv[1], "r", "utf-8")
+    f2 = codecs.open(argv[2], "w", "utf-8")
+
+
+    re_wrong_tags = re.compile("<\r\n")
+    re_wrong_tags2 = re.compile(u"<__NOTOC__")
+    re_wrong_tags3 = re.compile("<reference[^>]")
+    re_wrong_tags4 = re.compile("<re[^>]")
+    re_wrong_tags5 = re.compile("</noinclud[^>]")
+    re_wrong_tags6 = re.compile("<!--")
+    re_wrong_tags7 = re.compile(u"छ।<ज्योतिष")
+    re_inequation = re.compile("i<links\.length")
+    re_inequation2 = re.compile("<=//")
+    re_inequation3 = re.compile("<=\\\.")
+    re_inequation4 = re.compile("FF<3\.5, Opera <10\.5")
+   
+    
+    for line in f1:
+        line = re_wrong_tags.sub("\r\n", line)
+        line = re_wrong_tags2.sub("", line) 
+        line = re_wrong_tags3.sub("", line) 
+        line = re_wrong_tags4.sub("", line)
+        line = re_wrong_tags5.sub("", line)
+        line = re_wrong_tags6.sub("", line)
+        line = re_wrong_tags7.sub("", line)
+        line = re_inequation.sub("", line)
+        line = re_inequation2.sub("", line)
+        line = re_inequation3.sub("", line)
+        line = re_inequation4.sub("", line)
+        
+        f2.write(line)
+
+    f1.close()
+    f2.close()
+
+if __name__ == "__main__":
+    main(sys.argv)
