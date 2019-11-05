8fd30aace00442c9a7a11b9783ee0bf6c6a117f8
Peter Bouda <hey@peterbouda.eu>
2019-11-05 11:38:52 +0100

    Add task to build ngram models

--- prediction/update.py
+++ prediction/update.py
@@ -18,6 +18,7 @@ import codecs
 import re
 import pickle
 from tempfile import mkdtemp
+
 try:
     import configparser
 except ImportError:
@@ -28,6 +29,7 @@ import pressagio.dbconnector
 
 ###################################### Main
 
+
 def main(argv):
     arg_iso = None
     if len(argv) > 1:
@@ -36,42 +38,43 @@ def main(argv):
     script_path = os.path.dirname(os.path.realpath(__file__))
     os.chdir(script_path)
 
-    config_file = os.path.join('..', 'config.ini')
+    config_file = os.path.join("..", "config.ini")
     config = configparser.ConfigParser()
     config.read(config_file)
 
     processed = dict()
-    processed_file = os.path.join('..', 'build', 'processed.pickle')
+    processed_file = os.path.join("..", "build", "processed.pickle")
     if os.path.exists(processed_file):
-        with open(processed_file, 'rb') as f:
+        with open(processed_file, "rb") as f:
             processed = pickle.load(f)
 
-    if 'prediction' not in processed:
-        processed['prediction'] = dict()
+    if "prediction" not in processed:
+        processed["prediction"] = dict()
 
     corpus_dir = os.path.abspath(os.path.join("..", "build", "corpus"))
     prediction_dir = os.path.join("..", "build", "prediction")
     for iso_639_3 in config.options("LanguagesISOMap"):
-        
+
         if arg_iso and iso_639_3 != arg_iso:
             continue
 
-        separators_file = os.path.join(
-            "..", "build", "separators", "allchars.txt")
+        separators_file = os.path.join("..", "build", "separators", "allchars.txt")
         s_f = codecs.open(separators_file, "r", "utf-8")
         separators = s_f.read()
         s_f.close()
 
         dictionary = []
 
-        corpus_archives = glob.glob(os.path.join(corpus_dir, "{0}*.zip".format(iso_639_3)))
+        corpus_archives = glob.glob(
+            os.path.join(corpus_dir, "{0}*.zip".format(iso_639_3))
+        )
 
         for f in corpus_archives:
-            
+
             _, filename = os.path.split(os.path.abspath(f))
             filebasename, _ = os.path.splitext(filename)
 
-            print("Processing {0}".format(filename))
+            print ("Processing {0}".format(filename))
 
             try:
                 tmp_path = mkdtemp()
@@ -79,11 +82,12 @@ def main(argv):
                 z = zipfile.ZipFile(f)
                 z.extractall(tmp_path)
 
-                text_files = glob.glob(os.path.join(
-                    tmp_path,"{0}*.txt".format(filebasename)))
+                text_files = glob.glob(
+                    os.path.join(tmp_path, "{0}*.txt".format(filebasename))
+                )
 
                 if len(text_files) != 1:
-                    print("  something is wrong with the archive. Exiting.")
+                    print ("  something is wrong with the archive. Exiting.")
                     sys.exit(1)
 
                 text_file = text_files[0]
@@ -91,20 +95,25 @@ def main(argv):
                 if match:
                     wiki_date = match.group(1)
                 else:
-                    print("  Could not find date in corpus file. Exiting")
+                    print ("  Could not find date in corpus file. Exiting")
                     sys.exit(1)
 
-                if not arg_iso and iso_639_3 in processed['prediction'] and \
-                        int(processed['prediction'][iso_639_3]) >= int(
-                            wiki_date):
+                if (
+                    not arg_iso
+                    and iso_639_3 in processed["prediction"]
+                    and int(processed["prediction"][iso_639_3]) >= int(wiki_date)
+                ):
                     print "  Corpus files already processed, skipping."
                     continue
 
                 # build n-gramm statistics, requires pressagio:
                 # http://github.com/cidles/pressagio
                 for ngram_size in [1, 2, 3]:
-                    print("  Parsing {0} for cardinality {1}...".format(
-                        text_file, ngram_size))
+                    print (
+                        "  Parsing {0} for cardinality {1}...".format(
+                            text_file, ngram_size
+                        )
+                    )
 
                     cutoff = 0
                     if ngram_size == 3 and os.path.getsize(text_file) > 20000:
@@ -113,12 +122,19 @@ def main(argv):
                         cutoff = 1
 
                     ngram_map = pressagio.tokenizer.forward_tokenize_file(
-                        text_file, ngram_size, cutoff=cutoff)
+                        text_file, ngram_size, cutoff=cutoff
+                    )
 
-                    print("  Writing result to postgres database...")
-                    pressagio.dbconnector.insert_ngram_map_postgres(ngram_map,
-                        ngram_size, iso_639_3, append=False, create_index=True,
-                        lowercase=True, normalize=True)
+                    print ("  Writing result to postgres database...")
+                    pressagio.dbconnector.insert_ngram_map_postgres(
+                        ngram_map,
+                        ngram_size,
+                        iso_639_3,
+                        append=False,
+                        create_index=True,
+                        lowercase=True,
+                        normalize=True,
+                    )
 
             finally:
                 try:
@@ -126,8 +142,8 @@ def main(argv):
                 except WindowsError:
                     pass
 
-            processed['prediction'][iso_639_3] = wiki_date
-            with open(processed_file, 'wb') as f:
+            processed["prediction"][iso_639_3] = wiki_date
+            with open(processed_file, "wb") as f:
                 pickle.dump(processed, f)
 
 
--- tasks/update_models.py
+++ tasks/update_models.py
@@ -1,16 +1,19 @@
 import os
+import json
+import gc
 
 import luigi
 import poiolib.wikipedia
+import pressagio
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
-def corpus_path(iso_639_3):
-    return os.path.join(SCRIPT_DIR, "..", "build", "corpus", "wikipedia", iso_639_3)
+def build_path(iso_639_3, build_type):
+    return os.path.join(SCRIPT_DIR, "..", "build", build_type, iso_639_3)
 
 
-class WikipediaCorpusTask(luigi.Task):
+class WikipediaCorpus(luigi.Task):
     iso_639_3 = luigi.Parameter()
 
     def run(self):
@@ -19,5 +22,69 @@ class WikipediaCorpusTask(luigi.Task):
 
     def output(self):
         return luigi.LocalTarget(
-            os.path.join(corpus_path(self.iso_639_3), "{}.txt".format(self.iso_639_3))
+            os.path.join(build_path(self.iso_639_3, "corpus"), "wikipedia.txt")
         )
+
+
+class Ngrams(luigi.Task):
+    iso_639_3 = luigi.Parameter()
+    ngram_size = luigi.IntParameter()
+
+    def requires(self):
+        return WikipediaCorpus(self.iso_639_3)
+
+    def run(self):
+        text_file = self.input().fn
+        cutoff = 0
+        if self.ngram_size == 3 and os.path.getsize(text_file) > 20000:
+            cutoff = 1
+        if self.ngram_size == 2 and os.path.getsize(text_file) > 100000:
+            cutoff = 1
+
+        self.output().makedirs()
+        gc.collect()
+        ngram_map = pressagio.tokenizer.forward_tokenize_file(
+            text_file, self.ngram_size, cutoff=cutoff
+        )
+
+        pressagio.dbconnector.insert_ngram_map_postgres(
+            ngram_map,
+            self.ngram_size,
+            self.iso_639_3,
+            append=False,
+            create_index=True,
+            lowercase=True,
+            normalize=True,
+        )
+
+        try:
+            with open(self.output().fn, "w", encoding="utf-8") as f:
+                for ngram, count in ngram_map.items():
+                    f.write("{}\t{}\n".format("\t".join(ngram), count))
+        except Exception:
+            os.remove(self.output().fn)
+
+    def output(self):
+        return luigi.LocalTarget(
+            os.path.join(
+                build_path(self.iso_639_3, "ngrams"),
+                "{}gram.txt".format(self.ngram_size),
+            )
+        )
+
+
+class AllNgrams(luigi.Task):
+    iso_639_3 = luigi.Parameter()
+
+    def requires(self):
+        return [Ngrams(self.iso_639_3, ngram_size) for ngram_size in [1, 2, 3]]
+
+
+class AllLanguages(luigi.Task):
+    def requires(self):
+        with open(
+            os.path.join(SCRIPT_DIR, "..", "config.json"), "r", encoding="utf-8"
+        ) as f:
+            config = json.load(f)
+
+        return [AllNgrams(iso_639_3) for iso_639_3 in config["languages"]]
